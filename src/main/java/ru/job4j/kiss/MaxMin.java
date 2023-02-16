882490ec5f45c658d945b5d6eb4f237b079cf0d8
MaxSerbin <silverspoor644@gmail.com>
2023-02-16 19:16:06 +0300

    2. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,19 @@
+package ru.job4j.kiss;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return getValue(value, comparator.reversed());
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return getValue(value,comparator);
+    }
+
+    public <T> T getValue(List<T> value, Comparator<T> comparator) {
+        value.sort(comparator);
+        return value.get(0);
+    }
+}
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+
+class MaxMinTest {
+    private List<Integer> values = new ArrayList<>();
+    private Comparator<Integer> comp = Comparator.naturalOrder();
+
+    @BeforeEach
+    void initEach() {
+        values.add(1);
+        values.add(2);
+        values.add(3);
+    }
+
+    @Test
+    void whenMax() {
+        MaxMin maxmin = new MaxMin();
+        int rsl = maxmin.max(values,comp);
+        assertThat(rsl).isEqualTo(3);
+    }
+
+    @Test
+    void whenMin() {
+        MaxMin maxmin = new MaxMin();
+        int rsl = maxmin.min(values,comp);
+        assertThat(rsl).isEqualTo(1);
+    }
+}
