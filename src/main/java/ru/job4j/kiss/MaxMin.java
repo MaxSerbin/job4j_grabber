2ad6c25bc655c1f248ae6c8617d5e2f44a4ae5f1
MaxSerbin <silverspoor644@gmail.com>
2023-02-20 19:00:06 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 #392365]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -6,14 +6,17 @@ import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return  getValue(value, (o1, o2) -> comparator.compare(o1, o2) > 0);
+        return  getValue(value, (o1, o2) -> comparator.compare(o1, o2) >= 0);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return getValue(value, (o1, o2) -> comparator.compare(o1, o2) < 0);
+        return getValue(value, (o1, o2) -> comparator.compare(o1, o2) <= 0);
     }
 
     private  <T> T getValue(List<T> value, BiPredicate<T, T> predicate) {
+        if (value.isEmpty()) {
+            return null;
+        }
         var rsl = value.get(0);
         for (var val : value) {
             if (predicate.test(val, rsl)) {
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -32,4 +32,36 @@ class MaxMinTest {
         int rsl = maxmin.min(values, comp);
         assertThat(rsl).isEqualTo(1);
     }
+
+    @Test
+    void whenIsEmptyMin() {
+        MaxMin maxmin = new MaxMin();
+        values.clear();
+        var exp = maxmin.min(values, comp);
+        assertThat(exp).isEqualTo(null);
+    }
+
+    @Test
+    void whenIsEmptyMax() {
+        MaxMin maxmin = new MaxMin();
+        values.clear();
+        var exp = maxmin.min(values, comp);
+        assertThat(exp).isEqualTo(null);
+    }
+
+    @Test
+    void whenDoubleMax() {
+        MaxMin maxmin = new MaxMin();
+        values.add(3);
+        int rsl = maxmin.max(values, comp);
+        assertThat(rsl).isEqualTo(3);
+    }
+
+    @Test
+    void whenDoubleMin() {
+        MaxMin maxmin = new MaxMin();
+        values.add(1);
+        int rsl = maxmin.min(values, comp);
+        assertThat(rsl).isEqualTo(1);
+    }
 }
